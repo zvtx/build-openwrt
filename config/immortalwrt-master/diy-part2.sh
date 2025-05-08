01d15f9828e3dff762261a1978b413c6af9c74df
zvtx <49553428+zvtx@users.noreply.github.com>
2025-05-08 22:46:52 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -23,12 +23,12 @@ echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_r
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-#svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-zerotier
 svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-passwall
 svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-homeproxy 
-svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-theme-argon 
-svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-wechatpush
+# svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-theme-argon 
+ svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-wechatpush
 svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-lucky
 svn c ohttps://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-tailscale
 
